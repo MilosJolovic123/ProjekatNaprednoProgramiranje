70fea4586915b5b649662be29457866fcfdab71c
milos-jolovic <mj20200179@student.fon.bg.ac.rs>
2024-06-17 11:40:55 +0200

    Made the app.

---
+++ src/main/java/rs/ac/bg/fon/ai/naprednoProgramiranje/NaprednoProgramiranjeApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.fon.ai.naprednoProgramiranje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaprednoProgramiranjeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaprednoProgramiranjeApplication.class, args);
+	}
+
+}
